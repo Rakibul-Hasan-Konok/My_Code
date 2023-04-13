881a51b960f3af6d3dd5c3107286206e2304c1aa
RH Konok <rakibulhasankonok.ju@gmail.com>
2023-04-14 00:34:29 +0600

    Xor Problem

---
+++ CodeForce/MinSum.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int t;
+cin>>t;
+while(t--){
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    long long int OR=0;
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+        OR|=v[i];
+    } 
+    cout<<OR<<endl;
+
+}
+}
+
+
