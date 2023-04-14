9680759498ff98f088f258b7033808a9a100cd41
RH Konok <rakibulhasankonok.ju@gmail.com>
2023-04-14 23:37:48 +0600

    DataType of JS

---
+++ CodeForce/FairPlayoff.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int t;
+cin>>t;
+while (t--)
+{    vector<int> v(4),a;
+   for(int i=0;i<4;i++) cin>>v[i];
+   a=v;
+   sort(a.begin(),a.end());
+   int max1=max(v[0],v[1]);
+   int max2=max(v[2],v[3]);
+   if((max1==a[3] || max1==a[2]) && (max2==a[3] || max2==a[2]) ) cout<<"YES\n";
+   else cout<<"NO\n";
+
+}
+
+}
---
+++ Javascript/DataType.js
@@ -0,0 +1,11 @@
+var Num=1223;
+console.log(typeof Number);
+
+var nam="RH Konok";
+console.log(typeof nam);
+
+var num=1.3389;
+console.log(typeof num);
+
+var booll=true;
+console.log(typeof booll);
