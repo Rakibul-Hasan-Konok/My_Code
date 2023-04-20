52d2caf0f99ce09c3b80db90ffbc7fded51430ab
RH Konok <rakibulhasankonok.ju@gmail.com>
2023-04-21 01:12:50 +0600

    solve A

--- Contest/Matching.cpp
+++ Contest/Matching.cpp
@@ -6,30 +6,23 @@ int main(){
     while(t--){
         string s;
         cin>>s;
-        int k=0;
-        for(int i=0;i<s.size();i++){
-        if(s[i]='9' && s.size()==1) {
-            cout<<1<<endl;
-            k++;
-            break;}
-        else if(s[i]='0' && s.size()==1) {
+        int ans=1,k=0;
+        for(int i=0;i<s.size();i++)
+      {
+        if(s[0]=='0') {
             cout<<0<<endl;
             k++;
-            break;}
-        else if(s[i]=='?' && s[i+1]=='?') {
-            cout<<100<<endl;
-            k++;
-            break;}
-            else if(s[i]=='?' && s[i+1]!='?') {
-            cout<<90<<endl;
-            break;}
-            else{
-                k=0;
-            }
-
-        }
-        if(k==0) cout<<0<<endl;
-        
+             break;}
+        else if(s[0]=='?' && i==0)
+         {
+             ans*=9;
+         }
+         else
+         {
+            if(s[i]=='?') ans*=10;
+         }
+      }
+     if(k==0) cout<<ans<<endl;
         }
 
     }
