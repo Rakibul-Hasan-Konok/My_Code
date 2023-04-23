4bcf2c6945fe41d2b821885a907d6da792daa68a
RH Konok <rakibulhasankonok.ju@gmail.com>
2023-04-24 00:56:01 +0600

    MAX OR And Operation

--- CodeForce/OrAnd.cpp
+++ CodeForce/OrAnd.cpp
@@ -5,14 +5,14 @@ int main( )
    int t;
    cin>>t;
    while(t--){
-    int n,z;
+    int n,z,max=0;
     cin>>n>>z;
     int arr[n];
     for(int i=0;i<n;i++){
         cin>>arr[i];
+        if(max<(z|arr[i])) max=(z|arr[i]); 
     }
-     int max=*max_element(arr,arr+n);
-     if((max|z)>(max&z))cout<<(max|z)<<endl;
-     else cout<<(max&z)<<endl;
+     //int max=*max_element(arr,arr+n);
+     cout<<max<<endl;
    }
 }
