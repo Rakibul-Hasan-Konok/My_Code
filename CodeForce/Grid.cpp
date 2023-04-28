b566c29ff7ab96415c4c1759d0b002546b2684ec
RH Konok <rakibulhasankonok.ju@gmail.com>
2023-04-28 11:31:12 +0600

    Grid PBLM

---
+++ CodeForce/Grid.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main( )
+{
+    int t;
+    cin>>t;
+    while(t--){
+       long long int x,y;
+        cin>>x>>y;
+        if(abs(x-y)==1) {
+            cout<<1<<endl;
+            cout<<x<<" "<<y<<endl;
+            }
+        else{
+            if(x+1 > 1e9) 
+            {    cout<<2<<endl;
+                cout<<x-1<<" "<<1<<endl;
+                cout<<x<<" "<<y<<endl;
+            }
+            else {  cout<<2<<endl;
+                cout<<x+1<<" "<<1<<endl;
+                cout<<x<<" "<<y<<endl;
+            }
+        }
+}
+}
