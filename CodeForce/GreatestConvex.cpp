6216eb3e7bbe254e1896c45bfbf417e1a20a4fff
RH Konok <rakibulhasankonok.ju@gmail.com>
2023-05-04 22:49:03 +0600

    Indivisible

--- CodeForce/GreatestConvex.cpp
+++ CodeForce/GreatestConvex.cpp
@@ -8,8 +8,7 @@ while (t--)
    long long int n,sum=0;
     cin>>n;
     for(int i=1;i<n;i++) {
-        sum*=i;
-        }
+        sum*=i;}
     if(sum%n==0) cout<<n-1<<endl;
     else cout<<-1<<endl;
 }
---
+++ CodeForce/Indivisible.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+   int t;                  
+   cin>>t;
+   while(t--){
+      int n;               
+      cin>>n;
+
+      if(n==1){
+         cout<<1<<endl;
+         continue;
+      }
+
+      if(n%2!=0){
+         cout<<-1<<endl;
+         continue;
+      }
+
+      for(int i=2;i<=n;i+=2){
+         cout<<i<<" "<<(i-1)<<" ";
+      }
+      cout<<endl;
+   }
+   return 0;
+}
