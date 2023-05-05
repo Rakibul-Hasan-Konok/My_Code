6721ac7e4317a8842a5ad92978ec51f7dc3849db
RH Konok <rakibulhasankonok.ju@gmail.com>
2023-05-05 22:56:51 +0600

    HTML and CSS

--- Javascript/DataType.js
+++ Javascript/DataType.js
@@ -1,12 +1,23 @@
-// var Num=1223;
-// console.log(typeof Number);
-// var nam="RH Konok";
-// console.log(typeof nam);
-// var num=1.3389;
-// console.log(typeof num);
-// var booll=true;
-// console.log(typeof booll);
-var nm=Math.max(0, 150, 30, 20, -8, -200);
-console.log(nm);
-var x=4.49;
-console.log(Math.ceil(x));
+// // var Num=1223;
+// // console.log(typeof Number);
+// // var nam="RH Konok";
+// // console.log(typeof nam);
+// // var num=1.3389;
+// // console.log(typeof num);
+// // var booll=true;
+// // console.log(typeof booll);
+// var nm=Math.max(0, 150, 30, 20, -8, -200);
+// console.log(nm);
+// var x=4.49;
+// console.log(Math.ceil(x));
+// var shop={
+//     books : 3,
+//     bottle : 4,
+//     shoes : 2
+// }
+
+// console.log(shop);
+
+/*******************************************************************************************/
+
+
